f1d9917b491c3c1566fe419cd2d8428428b21666
nghiacanhtran <nghiacanhtran@gmail.com>
2023-12-29 11:08:45 +0700

    Create main.ps1

---
+++ main.ps1
@@ -0,0 +1,13 @@
+# khai bao bien nhan tu console screen
+# #name_root: name of root project or folder
+Param([string]$name_root)
+# khai bao array string
+$arrayFolderTemplate = @("01_URD","02_RSD", "03_POSTMAN", "04_PTTK", "05_Common")
+# Lay duong dan folder hien tai
+$currentPath =  Get-Location
+
+#Iterate through array folder name
+ForEach ($folder in $arrayFolderTemplate ) {
+    #Create Each Folder
+    New-Item -ItemType Directory -Force -Path "$($currentPath)\$($name_root)\$($folder)"
+}
